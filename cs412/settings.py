561f9989cf903e66110890eb8158fe7f003705c4
Decheng Zheng <dechengz@bu.edu>
2025-02-07 18:13:58 -0500

    c

--- cs412/settings.py
+++ cs412/settings.py
@@ -135,4 +135,4 @@ import socket
 CS_DEPLOYMENT_HOSTNAME = 'cs-webapps.bu.edu'
 
 if socket.gethostname() == CS_DEPLOYMENT_HOSTNAME:
-    STATIC_URL = '/dechengz/static/'  
+    STATIC_URL = '/dechengz/static'  
