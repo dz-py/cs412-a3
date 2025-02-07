ab40f9166ac495e24a582185560628d3ba3bc1e7
Decheng Zheng <dechengz@bu.edu>
2025-02-07 18:33:34 -0500

    c

--- cs412/settings.py
+++ cs412/settings.py
@@ -129,8 +129,7 @@ CS_DEPLOYMENT_HOSTNAME = 'cs-webapps.bu.edu'
 
 if socket.gethostname() == CS_DEPLOYMENT_HOSTNAME:
     STATIC_URL = '/dechengz/static/'
-else:
-    STATIC_URL = '/static/'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
@@ -140,6 +139,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 #declarations to reference to the static files
 #tells django where to look for static files
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/dechengz/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
